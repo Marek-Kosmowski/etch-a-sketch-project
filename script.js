00082542c1a9a44f32fcc1d84f0e246b043b4dc3
Marek-Kosmowski <kosmowski.marek@gmail.com>
2022-10-03 21:14:18 +0200

    Add function which when mouseover a square, add a new class to that square. Add in style.css 'fill' class with background different background color

--- script.js
+++ script.js
@@ -11,4 +11,16 @@ function createGrid() {
 
 }
 
+function paint() {
+    const paintSquare = document.querySelectorAll('.square')
+    paintSquare.forEach(el => {
+        el.addEventListener('mouseover', () => {
+            el.classList.add('fill')
+        })
+    })
+}
+
+// addEventListener('mouseover', paint)
+addEventListener('mouseover', paint);
+
 createGrid();
